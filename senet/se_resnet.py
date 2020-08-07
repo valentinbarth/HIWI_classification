eec29ace53a18348ea097e08e5f50ce69eecfe5c
vbarth <vbarth@medphys410>
2020-08-07 17:18:32 +0200

    num_classes = 1_000 --> 10 * entfernt

--- senet/se_resnet.py
+++ senet/se_resnet.py
@@ -12,8 +12,7 @@ class SEBasicBlock(nn.Module):
     expansion = 1
 
     def __init__(self, inplanes, planes, stride=1, downsample=None, groups=1,
-                 base_width=64, dilation=1, norm_layer=None,
-                 *, reduction=16):
+                 base_width=64, dilation=1, norm_layer=None, reduction=16):
         super(SEBasicBlock, self).__init__()
         self.conv1 = conv3x3(inplanes, planes, stride)
         self.bn1 = nn.BatchNorm2d(planes)
@@ -47,8 +46,7 @@ class SEBottleneck(nn.Module):
     expansion = 4
 
     def __init__(self, inplanes, planes, stride=1, downsample=None, groups=1,
-                 base_width=64, dilation=1, norm_layer=None,
-                 *, reduction=16):
+                 base_width=64, dilation=1, norm_layer=None, reduction=16):
         super(SEBottleneck, self).__init__()
         self.conv1 = nn.Conv2d(inplanes, planes, kernel_size=1, bias=False)
         self.bn1 = nn.BatchNorm2d(planes)
@@ -86,7 +84,7 @@ class SEBottleneck(nn.Module):
         return out
 
 
-def se_resnet18(num_classes=1_000):
+def se_resnet18(num_classes=10):
     """Constructs a ResNet-18 model.
 
     Args:
@@ -97,7 +95,7 @@ def se_resnet18(num_classes=1_000):
     return model
 
 
-def se_resnet34(num_classes=1_000):
+def se_resnet34(num_classes=10):
     """Constructs a ResNet-34 model.
 
     Args:
@@ -108,7 +106,7 @@ def se_resnet34(num_classes=1_000):
     return model
 
 
-def se_resnet50(num_classes=1_000, pretrained=False):
+def se_resnet50(num_classes=10, pretrained=False):
     """Constructs a ResNet-50 model.
 
     Args:
@@ -122,7 +120,7 @@ def se_resnet50(num_classes=1_000, pretrained=False):
     return model
 
 
-def se_resnet101(num_classes=1_000):
+def se_resnet101(num_classes=10):
     """Constructs a ResNet-101 model.
 
     Args:
@@ -133,7 +131,7 @@ def se_resnet101(num_classes=1_000):
     return model
 
 
-def se_resnet152(num_classes=1_000):
+def se_resnet152(num_classes=10):
     """Constructs a ResNet-152 model.
 
     Args:

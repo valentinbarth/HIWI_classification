d0677c45570cac795b0400d15e084c8538f87c9c
vbarth <vbarth@medphys410>
2020-08-26 17:46:48 +0200

    some corrections in the training inizialization

--- training.py
+++ training.py
@@ -6,14 +6,18 @@ import torch.optim as optim
 import torch.nn as nn
 import torch.nn.functional as F
 #from dataloading.dataset import MergedPatientDataset
-from training.cvtrainer import CVTrainer
-from training.trainer import Trainer
-import create_dataset
+
+from Training_custom.trainer import Trainer
+from Training_custom.cvtrainer import CVTrainer
+import Training_custom.create_dataset
+
+from senet.baseline import resnet20
+from senet.se_resnet import se_resnet20
 
 import logging
 
 #from models.unet import UNet3D, UNetCNN
-from utils.losses import KLDivMSE
+#from utils.losses import KLDivMSE
 
 # set device for training
 cuda_enabled_gpu = torch.cuda.is_available()
@@ -24,11 +28,11 @@ torch.backends.cudnn.benchmark = cuda_enabled_gpu
 # Global training parameter kwargs
 train_config = {
     'device': device,
-    'epochs': 100,
-    'batches_per_epoch': 3,
-    'batch_size': 30,
+    'epochs': 10,   #change to ~200 
+    'batches_per_epoch': 1400,
+    'batch_size': 64,
     'num_workers': 2,
-    'output_folder': 'test_run',
+    'output_folder': 'se_net_trained',
     'validation_split': 0.1,
     'validation_indices': [],
     'prefetch_validation': False,
@@ -39,10 +43,9 @@ train_config = {
 if __name__ == "__main__":
 
     # create pytorch dataset
-    dataset_imgwise = create_dataset.imagewise_dataset(datadir = '/home/vbarth/HIWI/classificationDataValentin/mixed_cropped/train')
+    dataset_imgwise = Training_custom.create_dataset.imagewise_dataset(datadir = '/home/vbarth/HIWI/classificationDataValentin/mixed_cropped/train')
     
-
-    dataset_tr.set_normalization()
+    #dataset_tr.set_normalization()
 
     #dataset_tr.set_normalization(
     #    {
@@ -54,14 +57,14 @@ if __name__ == "__main__":
 
     # set model, optimizer and loss criterion
     #model = UNet3D(fmaps_per_level=[16, 32, 64, 128])
-    model = se_resnet20(num_classes=4, reduction=args.reduction)  # choose se_resnet20 or resnet20 for example
+    model = se_resnet20(num_classes=4, reduction=16)  # choose se_resnet20 or resnet20 for example
 
     # optimizer
     #optimizer = optim.AdamW(model.parameters(), lr=5e-4, weight_decay=1e-3)
-    optimizer = optim.SGD(lr=1e-1, momentum=0.9, weight_decay=1e-4)
+    optimizer = optim.SGD(model.parameters(),lr=1e-1, momentum=0.9, weight_decay=1e-4)
     # lr scheduler
     #scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, patience=500, factor=0.2, min_lr=1e-6)
-    scheduler = optim.lr_scheduler.StepLR(80, 0.1)
+    scheduler = optim.lr_scheduler.StepLR(optimizer, 80, 0.1)
     
     # loss
     #criterion = (alpha=1., reduction='mean')

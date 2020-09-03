bb2b2d35738eab73dc9a68146b94e209f4cd6915
vbarth <vbarth@medphys410>
2020-09-03 12:49:07 +0200

    add comment

--- Docker/config_rootless_docker.sh
+++ Docker/config_rootless_docker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 curl -sSL https://get.docker.com/rootless | sh
-export PATH=/home/vbarth/bin:$PATH
+export PATH=/home/vbarth/bin:$PATH  #change to your user
 export PATH=$PATH:/sbin
-export DOCKER_HOST=unix:///run/user/1008/docker.sock
+export DOCKER_HOST=unix:///run/user/1008/docker.sock  #also change UID here
 systemctl --user start docker 
 # execute this file with ". config_rootless_docker.sh" without the ". " in the beginning it deos not work!!
